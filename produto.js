8b0438689a150ded74f6e924bcba20d6dd08c56a
Kewin5564 <kewin.silva.oliveira@escola.pr.gov.br>
2026-04-08 22:00:15 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,17 @@
+const nomeDoProduto = "Camiseta de algodão"
+let preçoDoProduto = 29.99
+const marcadoproduto = "marca x"
+preçoDoProduto = 39.99
+const produtocompleto = nomeDoProduto + " " + preçoDoProduto
+console.log(produtocompleto+preçoDoProduto)
+
+
+
+
+
+
+
+
+
+
+
